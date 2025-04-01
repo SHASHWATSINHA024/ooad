6e76c8933981783fd877c5c0783e0d56b685f7f0
Shashwat Sinha <shashwat.sinha024@gmail.com>
2025-04-01 21:56:57 +0530

    test2

--- src/main/java/com/librarymanagement/controller/UserController.java
+++ src/main/java/com/librarymanagement/controller/UserController.java
@@ -1,23 +1,24 @@
 package com.librarymanagement.controller;
 
-import com.librarymanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.librarymanagement.dto.UserDTO;
+import com.librarymanagement.service.UserService;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public String register(@RequestBody UserDTO userDTO) {
-        return userService.registerUser(userDTO);
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestBody UserDTO userDTO) {
-        return userService.loginUser(userDTO);
+    public String registerUser(@RequestBody UserDTO userDTO) {
+        userService.registerUser(userDTO);
+        return "User registered successfully!";  // ✅ Now returns a String message
     }
 }
